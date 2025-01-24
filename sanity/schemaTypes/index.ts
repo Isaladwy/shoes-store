0a92f3751f7475500592357c096b01f49e8e1c60
Isaladwy <isaladwy@gmail.com>
2025-01-24 21:01:52 +0200

    Add Sanity schema types for product, main product, and admin

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { product } from './product'
+import { mainProduct } from './mainProduct'
+import { admin } from './admin'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [product, mainProduct, admin],
 }
