d5c59d7436dad79e4c1387a7647e961ebcd25faf
Isaladwy <isaladwy@gmail.com>
2025-01-26 02:52:08 +0200

    Implement Hero component to display main product image and configure remote image loading

--- app/(app)/sections/Hero.tsx
+++ app/(app)/sections/Hero.tsx
@@ -1,8 +1,23 @@
 import getMainProduct from '@/services/product/MainProduct';
+import Image from 'next/image';
 import React from 'react';
 
 export default async function Hero() {
   const mainProduct = await getMainProduct();
   console.log(mainProduct);
-  return <div></div>;
+
+  return (
+    <div className="md:h-[70%] border-b mt-12">
+      <div className="w-full h-full md:grid md:grid-cols-2">
+        <div className="relative w-full md:h-full h-[400px] flex justify-center items-center">
+          <Image
+            src={mainProduct.image}
+            className="object-contain p-12"
+            alt="mainProduct"
+            fill
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
--- services/product/MainProduct.ts
+++ services/product/MainProduct.ts
@@ -1,11 +1,15 @@
+import { urlFor } from '@/sanity/lib/image';
 import { sanity } from '@/sanity/lib/sanity';
+import { Product } from '@/types/product';
 
 export default async function getMainProduct() {
   const query = `*[_type=='main_product'][0]{product->}`;
 
   const mainProduct = await sanity.fetch(query);
 
-  return mainProduct;
+  return prepareProduct(mainProduct.product);
 }
 
-function prepareProduct(product: Product){}
+function prepareProduct(product: Product) {
+  return { ...product, image: urlFor(product.image).url() } as Product;
+}
