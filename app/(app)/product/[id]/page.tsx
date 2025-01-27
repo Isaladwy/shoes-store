9d178e6f04e59eed17b2e03f9c88d5705a2bde97
Isaladwy <isaladwy@gmail.com>
2025-01-28 01:34:16 +0200

    Add product details and action buttons to product page

--- app/(app)/product/[id]/page.tsx
+++ app/(app)/product/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import { getProduct } from '@/services/product/MainProduct';
 import Image from 'next/image';
 import React from 'react';
@@ -20,6 +21,36 @@ export default async function Page({ params: { id } }: Params) {
           fill
           className="object-contain p-20"
         />
+      </div>
+      <div className="border-l border-l-zinc-200 h-full">
+        <div className="flex flex-col justify-center p-20 gap-4">
+          <span
+            style={{ color: product.color }}
+            className="text-5xl font-black p-4 border"
+          >
+            {product.title}
+          </span>
+          <span
+            style={{ color: product.color }}
+            className="text-4xl font-semibold"
+          >
+            {product.price}$
+          </span>
+          <div className="flex">
+            <Button
+              size="lg"
+              className="text-[17px] py-8 w-full bg-white hover:bg-zinc-100 border shadow-none text-zinc-700"
+            >
+              Add to cart
+            </Button>
+            <Button
+              size="lg"
+              className="text-[17px] py-8 w-full"
+            >
+              Buy now
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
