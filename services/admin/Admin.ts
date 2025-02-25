5635354662bec917a4a8f3e33c2f3f04aa91ad5a
Isaladwy <isaladwy@gmail.com>
2025-02-25 02:43:49 +0200

    modifiying middleware

--- services/admin/Admin.ts
+++ services/admin/Admin.ts
@@ -8,13 +8,13 @@ const sKey = process.env.NEXT_PUBLIC_JWT_SECRET_KEY || '';
 export type Admin = { email: string; password: string };
 
 export async function handleLogin(admin: Admin) {
-  const token = jwt.sign(admin, sKey); 
+  const token = jwt.sign(admin, sKey);
   (await cookies()).set('admin_token', token);
-  redirect('/admin')
+  redirect('/admin');
 }
 
 export async function handleAdmin(admin: Admin) {
-  const query = `*[_type=="admin" && password=="${admin.password}" && email=="${admin.email}"][0]`
+  const query = `*[_type=="admin" && password=="${admin.password}" && email=="${admin.email}"][0]`;
   const res = await sanity.fetch(query);
   console.log(res);
   return !!res as boolean;
